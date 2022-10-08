969bdaad191e79c20f0d67fe1109f6112369d11c
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-10-08 14:04:14 +0300

    done task

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,26 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import MoviesList from "./MoviesList";
+import React from 'react';
+// import the custom `useRouteMatch` hook from React Router
+import { Route, useRouteMatch } from 'react-router-dom';
+import MoviesList from './MoviesList';
+// import the MovieShow component
+import MovieShow from './MovieShow';
 
 function MoviesPage({ movies }) {
+  
+  console.log(match);
+
   return (
     <div>
       <MoviesList movies={movies} />
+      {/* 
+        we can use the current URL from the `match` object as part of the path;
+        this will generate a url like "/movies/:movieId"
+      */}
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow />
+      </Route>
     </div>
   );
 }
+
 export default MoviesPage;
